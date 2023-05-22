c91083b7978f531d1cdb5df02da82d7e7ac3a095
Davide <93427056+DavideDiCaprio@users.noreply.github.com>
2023-05-22 10:08:57 +0200

    Update CreateDB.java
    syntax fix

--- CreateDB.java
+++ CreateDB.java
@@ -9,9 +9,9 @@ public class CreateDB {
         try {
             String url = "jdbc:mysql://localhost:3306/";
 
-            String databaseName = "PersonDB";
-            String userName = "root";
-            String password = "gatesss";
+            String databaseName = "*";
+            String userName = "*";
+            String password = "*";
     
             Connection connection = DriverManager.getConnection(url,userName, password);
 

4994c73bc10bb5a669ad87d6928f84cbd24a8eb5
Davide <93427056+DavideDiCaprio@users.noreply.github.com>
2023-05-25 16:27:45 +0200

    Update PersonDBManager.java
    syntax fix

--- PersonDB/PersonDBManager.java
+++ PersonDB/PersonDBManager.java
@@ -10,46 +10,39 @@ public class PersonDBManager {
 	private String password = "*";
 	
 	public PersonDBManager(String url,String userName, String password) {
-		
 		this.url = url;
 		this.userName = userName;
-		this.password = password;
-		
-		}
+		this.password = password;	
+	}
 	
 	public void writePerson(Person p) {
-		
 		try(Connection conn = DriverManager.getConnection(this.url, this.userName, this.password);
-				
-				Statement stmt = conn.createStatement();
-				) {
-			
+		    Statement stmt = conn.createStatement();)
+		{
 			String sql_command = "INSERT INTO Person(Name, Surname, Age, Gender) VALUES(" 
-							   "'" + p.getName() +
-					           "','" + p.getSurname() +
-					           "','" + p.getAge() +
-					           "','" + p.getGender() + "')";
+				"'" + p.getName() +
+				"','" + p.getSurname() +
+				"','" + p.getAge() +
+				"','" + p.getGender() + "')";
 			
 			stmt.executeUpdate(sql_command);
 			System.out.println("Insert value in table Person...");
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
-			}
-		
+		}	
 	}
-	   
-	   public void readPerson(int PersonID) {
-		   try(Connection conn = DriverManager.getConnection(this.url, this.userName, this.password);
-				   Statement stmt = conn.createStatement();
-				   ) {
-			   String sql_command = "SELECT * FROM Person WHERE PersonID =" + PersonID + ";";
-			   stmt.executeQuery(sql_command);
-			   System.out.println("Select " + PersonID + " user.");
-			   }
-		   
-		   catch (SQLException e) {
+	
+	public void readPerson(int PersonID) {
+		try(Connection conn = DriverManager.getConnection(this.url, this.userName, this.password);
+		    Statement stmt = conn.createStatement();)
+		{
+			String sql_command = "SELECT * FROM Person WHERE PersonID =" + PersonID + ";";
+			stmt.executeQuery(sql_command);
+			System.out.println("Select " + PersonID + " user.");
+		}
+		catch (SQLException e) {
 			   e.printStackTrace();
-			   } 
-	   }
+		} 
+	  }
 }

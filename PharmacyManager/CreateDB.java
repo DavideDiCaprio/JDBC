11d552953fafe2e41d68c08daf038087833bb069
Davide <93427056+DavideDiCaprio@users.noreply.github.com>
2023-05-25 15:45:22 +0200

    Update CreateDB.java
    syntax fix

--- PharmacyManager/CreateDB.java
+++ PharmacyManager/CreateDB.java
@@ -9,24 +9,21 @@ public class CreateDB {
 		
 		try {
 			String url = "jdbc:mysql://localhost:3306/";
-
-            String databaseName = "PharmacyManager";
-            String userName = "root";
-            String password = "gatesss";
-    
-            Connection connection = DriverManager.getConnection(url,userName, password);
-
-    
-            String sql = "CREATE DATABASE IF NOT EXISTS " + databaseName;
-    
-            Statement statement = connection.createStatement();
-            statement.executeUpdate(sql);
-            statement.close();
-            JOptionPane.showMessageDialog(null, databaseName + " Database has been created successfully", "System Message", JOptionPane.INFORMATION_MESSAGE);
-            
-            
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+			String databaseName = "PharmacyManager";
+			String userName = "root";
+			String password = "";
+			
+			Connection connection = DriverManager.getConnection(url,userName, password);
+			
+			String sql = "CREATE DATABASE IF NOT EXISTS " + databaseName;
+			
+			Statement statement = connection.createStatement();
+			statement.executeUpdate(sql);
+			statement.close();
+			JOptionPane.showMessageDialog(null, databaseName + " Database has been created successfully", "System Message", JOptionPane.INFORMATION_MESSAGE);
+		
+		    } catch (Exception e) {
+			e.printStackTrace();
+		    }
+	}
 }

feababa373fe16fc40858ed722d401b5e44c67f4
Davide <93427056+DavideDiCaprio@users.noreply.github.com>
2023-05-26 11:31:16 +0200

    Update PharmacyManager.java
    syntax update

--- PharmacyManager/PharmacyManager.java
+++ PharmacyManager/PharmacyManager.java
@@ -12,43 +12,45 @@ public class PharmacyManager {
 	private String userName = "";
 	private String password = "";
 	
+	//
 	public void makePurchase(ArrayList<Integer> IDList, ArrayList<Integer> AmountList) {
 		
 		for (int i = 0; i < IDList.size(); i++) {
 			try(Connection conn = DriverManager.getConnection(url, userName, password);
-			    Statement stmt = conn.createStatement();) 
+			    Statement stmt = conn.createStatement();)
 			{
-				String sql_command = "SELECT price,amount From products WHERE ID = " + IDList.get(i) + ";";
+				String sql_command = "SELECT price, amount FROM products WHERE ID = " + IDList.get(i) +";";
+				stmt.executeQuery(sql_command);
 				ResultSet rs = stmt.executeQuery(sql_command);
 				
+				float price = 0;
+				int amount = 0;
+				
 				while (rs.next()) {
-					if (rs.wasNull()) {
-						System.out.println("Result set is null");
-					} else {
-						int price = rs.getInt("price");
-						int amount = rs.getInt("amount");
-						int newAmount = amount - AmountList.get(i);
-						
-						String sql_update = "UPDATE products SET amount =" + newAmount + "WHERE ID =" + IDList.get(i) + ";";
-						stmt.executeUpdate(sql_update);
-						
-						System.out.println("Total: " + price * AmountList.get(i));
-					}
+					price = rs.getFloat("price");
+					amount = rs.getInt("amount");
 				}
-							
+				
+				System.out.println("Total spent: " + price * AmountList.get(i));
+				int newAmount = amount - AmountList.get(i);
+				
+	    		String sql_command1 = "UPDATE products SET amount = " + newAmount + " WHERE ID =" + IDList.get(i) + ";";
+	    		System.out.println(sql_command1);
+	    		stmt.executeUpdate(sql_command1);
+				
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
-		}
+		}	
 	}
 	
+	//
 	public void checkProduct(int ID) throws SQLException {
-		
 		try(Connection conn = DriverManager.getConnection(url, userName, password);
-				Statement stmt = conn.createStatement();) 
+		    Statement stmt = conn.createStatement();) 
 		{
-			String sql_command = "SELECT * From products WHERE ID= " + ID +";" ;
-			
+			String sql_command = "SELECT * From products WHERE ID =" + ID +";";
+			stmt.executeQuery(sql_command);
 			ResultSet rs = stmt.executeQuery(sql_command);
 					
 			while (rs.next()) {
@@ -57,38 +59,36 @@ public class PharmacyManager {
 		          System.out.println("Format: " + rs.getString("Format"));
 		          System.out.println("Price: " + rs.getFloat("Price"));
 		          System.out.println("Amount: " + rs.getInt("Amount"));
-			}
-		}
-	}
+		   }
+	    }
+   }
 	
-	public void refill(int ID, int refillAmount) throws SQLException {
-    	
-    	System.out.println("ID = " + ID + "amount =" + refillAmount);
-		
-		try(Connection conn = DriverManager.getConnection(url, userName, password);
-				Statement stmt = conn.createStatement();)
-		{
-			String sql_command = "SELECT Amount FROM Products WHERE ID = " + ID +";";
-			ResultSet rs = stmt.executeQuery(sql_command);
-			
-			while (rs.next()) {
-				if (rs.wasNull()) {
-					System.out.println("Result set is null");
-				} else {
-					int amount = rs.getInt("amount");
-					System.out.println("amount =" + rs.getInt("amount"));
-					
-					String sql_command1 = "UPDATE products SET amount =" + (refillAmount + amount) + "WHERE ID = " + ID + ";";
-					stmt.executeUpdate(sql_command1);
-				}
-			}
-						
-		} catch (SQLException e) {
-			e.printStackTrace();
-		  }
-			 
-	}
-       
+   //
+   public void refill(int ID, int refillAmount) throws SQLException {
+	   
+	   try(Connection conn = DriverManager.getConnection(url, userName, password);
+	       Statement stmt = conn.createStatement();)
+	   {
+		   String sql_command = "select amount from products WHERE ID = " + ID + ";";
+		   stmt.executeQuery(sql_command);
+		   ResultSet rs = stmt.executeQuery(sql_command);
+		   int amount = 0;
+		   
+		   while (rs.next()){
+			   amount = rs.getInt("amount");
+		   }
+		   
+		   int newAmount = refillAmount + amount;
+		   System.out.println("refill amount: " + newAmount);
+		   
+		   String sqlUpdate = "UPDATE products SET amount = " + newAmount + " WHERE ID =" + ID + ";";
+		   stmt.executeUpdate(sqlUpdate);
+    		
+		}
+	   catch (SQLException e) {
+		   e.printStackTrace();
+	   }
+   } 
 	
 	public void start() throws SQLException {
 		
@@ -100,41 +100,37 @@ public class PharmacyManager {
 		String op = myScanner.nextLine();
 		
 		if(op.equals("purchase")) {
-			
 			while (true) {
-				
 				System.out.println("enter '0' to exit choise menu, or enter ID:");
 				int inputID = myScanner.nextInt();
 				
 				if (inputID == 0) {
 					
-					System.out.println("choise terminated.Purchase loading...");
-					if (IDList.size() == amountList.size()) {
+					System.out.println("choicve terminated.Purchase loading...");
+					if (IDList.size() == amountList.size()) {		
 						makePurchase(IDList,amountList);
-						
+						break;
 					} else {
 						System.out.println("different size list");
 						break;
 					}					
 				}
 								
-				if (inputID != 0);{
-					
+				else if (inputID != 0);{
 					IDList.add(inputID);
-					
 					System.out.println("enter amount: ");
 					int inputAmount = myScanner.nextInt();
-					
 					amountList.add(inputAmount);
 				}
 			}
+			
 			
 	    } else if (op.equals("check")) {
 	    	System.out.println("enter ID: ");
 			int inputID = myScanner.nextInt();
 			checkProduct(inputID);
 			
-		} else if (op.equals("refill")) {
+	    } else if (op.equals("refill")) {
 	    	
 			System.out.println("enter ID: ");
 			int inputID = myScanner.nextInt();
@@ -143,6 +139,6 @@ public class PharmacyManager {
 			int refilAmount = myScanner.nextInt();
 			
 			refill(inputID, refilAmount);
-		}  	
+		}
 	}
-}		
+}

cd2c1a7201f5e054db5bc87b5c0e08cdb382a3f2
Davide <93427056+DavideDiCaprio@users.noreply.github.com>
2023-05-26 11:32:14 +0200

    Update InsertTable.java
    syntax update

--- PharmacyManager/InsertTable.java
+++ PharmacyManager/InsertTable.java
@@ -5,17 +5,15 @@ import java.sql.Statement;
 
 public class InsertTable {
 	
-	private static String url = "jdbc:mysql://localhost:3306/PharmacyManager";
-	private static String userName = "root";
-	private static String password = "gatesss";
+	private static String url = "jdbc:mysql://localhost:3306/";
+	private static String userName = "";
+	private static String password = "";
 	
 	public static void main(String[] args) {
 		
 		try(Connection conn = DriverManager.getConnection(url, userName, password);
 				Statement stmt = conn.createStatement();) 
-		
 		{
-			//Insert into table 
 			String sql_command = "INSERT INTO Products (Name,Format,Price,Amount) VALUES('','','','')";
 			//
 			stmt.executeUpdate(sql_command);
@@ -27,4 +25,3 @@ public class InsertTable {
 	}
 			
 }
-

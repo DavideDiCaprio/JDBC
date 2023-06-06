263b441a73818ee6b227a7e33ba761f62dfa4fdd
Davide <93427056+DavideDiCaprio@users.noreply.github.com>
2023-06-06 16:48:35 +0200

    Update Product.java
    syntax fix

--- PharmacyManager/Product.java
+++ PharmacyManager/Product.java
@@ -3,8 +3,8 @@ import javax.print.attribute.standard.MediaSize.Other;
 import org.junit.jupiter.api.Order;
 
 public class Product {
-  
-  public int id;
+	
+	public int id;
 	public String name;
 	public String format;
 	public float price;
@@ -17,44 +17,43 @@ public class Product {
     this.format = format;
     this.price = price;
     this.amount = amount;
+	  
   }
-  
+	
   @Override
-	public boolean equals(Object obj) {
-    
-    if (obj == null) {
-      return false;
-		}
-    
-		if (obj.getClass() != this.getClass()) {
-      return false;
-		}
-    
-    Product other = (Product) obj;
-    
-		if (this.id != other.id) {
-      return false;
-		} 
-
-		if (!(this.name.equals(other.name))) {
-			return false;  
-		}  
-		 
-		if (!(this.format.equals(other.format))) {
-			return false;
-		}  
-		
-		if (this.price != other.price) {
-			return false;
-		} 
-		
-		if (this.amount != other.amount) {
-			return false;
-		} 
-    
-		return true;
-	}
-  
+  public boolean equals(Object obj) {
+	  
+	  if (obj == null) {
+		  return false;
+	  }
+	  
+	  if (obj.getClass() != this.getClass()) {
+		  return false;
+	  }
+	  
+	  Product other = (Product) obj;
+	  
+	  if (this.id != other.id) {
+		  return false;
+	  } 
+	  
+	  if (!(this.name.equals(other.name))) {
+		  return false;  
+	  }
+	  
+	  if (!(this.format.equals(other.format))) {
+		  return false;
+	  }
+	  
+	  if (this.price != other.price) {
+		  return false;
+	  }
+	  
+	  if (this.amount != other.amount) {
+		  return false;
+	  }
+	  return true;
+  }
 	@Override
 	public String toString() {
 		return "--- \n" + this.id +"\n"+this.name +"\n"+this.format +"\n"+this.price +"\n"+this.amount ;

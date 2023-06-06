65609ee831a9a93cb8301b5549052b6c7cb06593
Davide <93427056+DavideDiCaprio@users.noreply.github.com>
2023-06-06 19:29:27 +0200

    Update PharmacyManager.java

--- PharmacyManager/PharmacyManager.java
+++ PharmacyManager/PharmacyManager.java
@@ -12,13 +12,16 @@ public class PharmacyManager {
 	private String userName = "";
 	private String password = "";
 	
-	public void makePurchase(ArrayList<Integer> IDList, ArrayList<Integer> AmountList) {
+	@SuppressWarnings("null")
+	public float makePurchase(ArrayList<Integer> IDList, ArrayList<Integer> AmountList) {
+				
+		Float bill = 0.0f;
+		String productName = null;
+		Float price = 0.0f;
+		int amount = 0;
 		
 		for (int i = 0; i < IDList.size(); i++) {
-			
-			String productName = null;
-			float price = 0;
-			int amount = 0;
+						
 			ResultSet rs = null;
 			
 			try(Connection conn = DriverManager.getConnection(url, userName, password);
@@ -31,7 +34,7 @@ public class PharmacyManager {
 				if (!rs.isBeforeFirst()){
 					System.out.println("The following ID: " + IDList.get(i) + " can not be found in the database.");
 				    System.out.println("");
-				    return;
+				    return (Float) null;
 				}
 
 				while (rs.next()) {
@@ -44,22 +47,27 @@ public class PharmacyManager {
 				// 
 				System.out.println("ECCEZIONE CATTURATA DAL TRY");
 				e.printStackTrace();
-				return;
+				return (Float) null;
+				
 			}
 
 			if (AmountList.get(i) <= 0) {
 				System.out.println("Amount must be greater than 0. " +  AmountList.get(i) + " not usable.");
-				return;
+				return (Float) null;
 			}
+			
+			bill = price * AmountList.get(i);
+			
 			System.out.println("You selected product: " + productName);
-			System.out.println("Total spent: " + price * AmountList.get(i));
+			System.out.println("Total spent: " + bill);
 			System.out.println("");
+			
 
 			if (amount <= AmountList.get(i)) {
 				System.out.println("Amount not available.");
 				System.out.println("");		
 				System.out.println(productName + "is no longer available. Max amount: " + amount + ".");		
-				return;
+				return (Float) null;
 			}
 
 			int newAmount = amount - AmountList.get(i);
@@ -75,11 +83,19 @@ public class PharmacyManager {
 	    	catch (SQLException e) {
 	    		e.printStackTrace();	    			
 	    		}
-		}	
+		}
+				
+		return bill;			
 	}
 	
-	//
-	public void checkProduct(int ID) throws SQLException {
+	
+	public Product checkProduct(int ID) throws SQLException {
+			
+		int id = 0;
+		String name = null;
+		String format = null;
+		Float price = 0.0f;
+		int amount = 0;
 		
 		try(Connection conn = DriverManager.getConnection(url, userName, password);
 				Statement stmt = conn.createStatement();) 
@@ -91,66 +107,97 @@ public class PharmacyManager {
 			if (!rs.isBeforeFirst()){
 				System.out.println("The following ID: " + ID + " can not be found in the database.");
 		        System.out.println("");
-		        return;
+		        return null;
 			}
-		
+						
 			while (rs.next()) {
-		          System.out.println("ID: " + rs.getInt("ID"));
-		          System.out.println("Name: " + rs.getString("Name"));
-		          System.out.println("Format: " + rs.getString("Format"));
-		          System.out.println("Price: " + rs.getFloat("Price"));
-		          System.out.println("Amount: " + rs.getInt("Amount"));
-		          System.out.println("");
-		   }
+				id = rs.getInt("ID");
+				name = rs.getString("Name");
+				format = rs.getString("Format");
+				price = rs.getFloat("Price");
+				amount = rs.getInt("Amount");
+		   }		
+	
 	    } catch (SQLException e) {
 	    	e.printStackTrace();
 		}
+		
+		Product p = new Product(id,name,format,price,amount);
+		return p;
    }
 	
 	
-   public void refill(int ID, int refillAmount) throws SQLException {
-    	
-    	try(Connection conn = DriverManager.getConnection(url, userName, password);
+   public Product refill(int ID, int refillAmount) throws SQLException {
+	   
+	   String name = null;
+	   String format = null;
+	   int nwAmount = 0;
+	   Float price = 0.0f;
+	   
+	   try(Connection conn = DriverManager.getConnection(url, userName, password);
     			Statement stmt = conn.createStatement();)
 		{
-    		String sql_command = "SELECT name,amount FROM products WHERE ID = " + ID + ";";
-			stmt.executeQuery(sql_command);
-			ResultSet rs = stmt.executeQuery(sql_command);
-			
-			String productName = null;
-			int amount = 0;
-			
-			if (!rs.isBeforeFirst()){
-				System.out.println("The following ID: " + ID + " can not be found in the database.");
-		        System.out.println("");
-		        return;
-			}
-			
-			while (rs.next()) {
-				productName = rs.getString("name");
-				amount = rs.getInt("amount");
-			}
-			
-			if (refillAmount <= 0) {
-				System.out.println("Amount must be greater than 0. " +  refillAmount + "not usable.");
-				return;
-			}
-			
-			int newAmount = refillAmount + amount;
-			System.out.println("Old amount : " + amount + "updating...");
-	        System.out.println("");
-			System.out.println("New amount for: " + productName + " is " + newAmount);
-			
-    		String sql_command1 = "UPDATE products SET amount = " + newAmount + " WHERE ID =" + ID + ";";
-    		stmt.executeUpdate(sql_command1);
-
+		   String sql_command = "SELECT name,amount FROM products WHERE ID = " + ID + ";";
+		   stmt.executeQuery(sql_command);
+		   ResultSet rs = stmt.executeQuery(sql_command);
+		   
+		   String productName = null;
+		   int amount = 0;
+		   
+		   if (!rs.isBeforeFirst()) {
+			   System.out.println("The following ID: " + ID + " can not be found in the database.");
+			   System.out.println("");
+			   }
+		   
+		   while (rs.next()) {
+			   productName = rs.getString("name");
+			   amount = rs.getInt("amount");
+			   }
+		   
+		   if (refillAmount <= 0) {
+			   System.out.println("Amount must be greater than 0. " +  refillAmount + "not usable.");
+				return null;
+			   }
+		   
+		   int newAmount = refillAmount + amount;
+		   System.out.println("Old amount : " + amount + "updating...");
+		   System.out.println("");
+		   System.out.println("New amount for: " + productName + " is " + newAmount);
+		   
+		   String sql_command1 = "UPDATE products SET amount = " + newAmount + " WHERE ID =" + ID + ";";
+		   stmt.executeUpdate(sql_command1);
+		   
 		}
-    	
-    	catch (SQLException e) {
-    		e.printStackTrace();
-		}	
-	} 		
-	
+	   
+	   catch (SQLException e) {
+		   e.printStackTrace();
+		   }
+	   
+	   try (Connection conn = DriverManager.getConnection(url, userName, password);
+			   Statement stmt = conn.createStatement();) {
+		   
+		   String sqlQuery = "SELECT * FROM products WHERE ID =" + ID + ";";
+		   stmt.executeQuery(sqlQuery);
+		   ResultSet rs = stmt.executeQuery(sqlQuery);
+		   
+		   while (rs.next()) {
+			   name = rs.getString("Name");
+			   format = rs.getString("Format");
+			   nwAmount = rs.getInt("amount");
+			   price = rs.getFloat("price");
+			   }
+		   }
+	   
+	   catch (SQLException e) {
+		   e.printStackTrace();
+		   }
+	   
+	   
+	   Product p = new Product(ID,name,format,price,nwAmount);
+	   return p;	
+		
+   }
+
 	public void start() throws SQLException {
 		
 		ArrayList<Integer> IDList = new ArrayList<Integer>();
@@ -161,6 +208,7 @@ public class PharmacyManager {
 			Scanner myScanner = new Scanner(System.in);
 			System.out.println("Select operation: purchase, check, refill ... ");
 			String op = myScanner.nextLine();	
+			
 			
 			if(op.equals("purchase")) {
 				
@@ -204,6 +252,10 @@ public class PharmacyManager {
 				refill(inputID, refilAmount);
 		    		    	
 		    }
+			
+			System.out.println("Closing Scanner...");
+			myScanner.close();
+			System.out.println("Scanner Closed.");
 		}	
 	}
 }
